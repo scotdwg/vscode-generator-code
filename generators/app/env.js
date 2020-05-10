093401dc3afbb67e5d13a231dd7e7f34cd24e3c1
Scott Weeden <scott@websharpstudios.com>
2020-05-10 15:26:32 -0700

    Update env.js

--- generators/app/env.js
+++ generators/app/env.js
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Copyright (C) Harper Corporation. All rights reserved.
  *--------------------------------------------------------*/
 'use strict';
 var request = require('request-light');
